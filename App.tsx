e411041d3286cb4985baeed463b3c36a1da5430b
Bill Wohlers <wohlers.w@northeastern.edu>
2022-07-29 18:34:50 -0400

    Basic data stuff done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useImportFonts } from "./hooks/useFonts";
+import * as SplashScreen from 'expo-splash-screen';
+import { SGText } from "./elements/SGText";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const appLoaded = useImportFonts();
+
+  useEffect(() => {
+    if (appLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [appLoaded])
+
+  if (!appLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <SGText>Open up App.tsx to start working on your app!</SGText>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
